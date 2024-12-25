91543778d397259b3a9fe5423baf7f9c1b5709d1
nicholasbrar <153023837+nicholasbrar@users.noreply.github.com>
2024-12-24 22:50:45 -0500

    Create 268-Missing-Number.cpp

---
+++ solutions/268-Missing-Number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        for(int i = 0; i <= nums.size(); i++){
+            if(std::find(nums.begin(), nums.end(), i) == nums.end()){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
